75482b6d3d3e4f78dc90eb9cb2039f9b17e91d74
priyathamsetti1 <157573416+priyathamsetti1@users.noreply.github.com>
2025-07-28 17:23:36 +0530

    Delete api.js

--- api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = 'http://localhost:5000';
-
-export const uploadImageAndPredict = async (image) => {
-  const formData = new FormData();
-  formData.append('image', image);
-  return await axios.post(`${API_URL}/predict`, formData);
-};
